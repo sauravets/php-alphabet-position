2244e9940f0081e2bc7b34c21a069f46d0e2e5c0
sauravets <saurav.dubey.ets@gmail.com>
2022-04-30 13:29:05 +0530

    #30

--- handle.php
+++ handle.php
@@ -1,23 +1,36 @@
 <?php
-    if (isset($_POST['submit'])) {
-        $alphabet = $_POST['alphabet'];
-        $upperArr = range('A', 'Z');
-        $lowerArr = range('a', 'z');
-       
-        if (ctype_upper($alphabet)) {
-            echo 'Position of ' . $alphabet .' is:' . (array_search($alphabet, $upperArr) + 1);
-        } 
-        elseif(ctype_lower($alphabet)) {
-            echo 'Position of ' . $alphabet .' is:' . (array_search($alphabet, $lowerArr) + 1);
-        }
-        else{
-            echo "not allowed";
-        }
-        //other option-
-    //     echo "<br><br>";
-    //     echo "<h2> Position of alphabet </h2>";
-    //     foreach(range('A','Z') as $str){
-    //         echo 'Position of ' . $str .' is:' . (array_search($str, $upperArr) + 1) .'<br><br>' ;            
-    //     }
+if (isset($_POST['submit'])) {
+    $alphabet = $_POST['alphabet'];
+    // $upperArr = range('A', 'Z');
+    // $lowerArr = range('a', 'z');
+
+    // if (ctype_upper($alphabet)) {
+    //     echo 'Position of ' . $alphabet .' is:' . (array_search($alphabet, $upperArr) + 1);
+    // } 
+    // elseif(ctype_lower($alphabet)) {
+    //     echo 'Position of ' . $alphabet .' is:' . (array_search($alphabet, $lowerArr) + 1);
+    // }
+    // else{
+    //     echo "not allowed";
+    // }
+    
+    // Declare an empty array
+    $array = array();
+
+    // Loop to convert ASCII value
+    // into characters and store
+    // it into variables
+    for ($i = 65; $i < 91; $i++) {
+        $array[$alphabet] = chr($i);
     }
-?>
+    
+
+    // Loop to display the array elements
+    foreach ($array as $alphabet => $v) {
+
+        // Display the key and its
+        // value of an array 
+        echo $alphabet . " => " . $v . ", ";
+    }
+
+}
