99aa9b73c28d2c9f61629592462f9a2b4d6fe6b4
sauravets <saurav.dubey.ets@gmail.com>
2022-05-04 11:48:24 +0530

    complete

--- handle.php
+++ handle.php
@@ -2,12 +2,18 @@
 if (isset($_POST['submit'])) {
     $alphabet = $_POST['alphabet'];
     $alphabet_length = strlen($alphabet);
+// $result = '';
+ echo '<div>';
     for ($i = 0; $i < $alphabet_length; $i++) {
         $ascii_value =  ord(strtoupper($alphabet[$i]));
         if ($ascii_value > 64 && $ascii_value < 91) {
-            echo "The position of " . $alphabet[$i] . " is: " . $ascii_value . "<br><br>";
+            $result = $ascii_value-64;
+          //  echo var_dump($result);
+            echo  "<li> the index of '".$alphabet[$i]."' is:".$result."</br></br></li>";
+            // echo "The position of " . $alphabet[$i] . " is: " . $ascii_value . "<br><br>";
         } else {
-            echo "not allowed<br><br>";
-        }
+            echo  "<li> the index of '".$alphabet[$i]."' is not Alphabet</br></li>";
+               }
     }
+    echo '</div>';
 }
